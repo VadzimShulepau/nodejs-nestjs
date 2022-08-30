3ed74faae4cbccb0c92a5faa7ddc6c95dcef3331
VadzimShulepau <shulepau.vadzim@gmail.com>
2022-08-31 00:07:01 +0300

    feat auth module, add methods with auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthDto } from './dto/auth.dto';
+import { UserEntity } from '../user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  async signup(authDto: AuthDto): Promise<UserEntity> {
+    return;
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async login(authDto: AuthDto): Promise<UserEntity> {
+    return;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  refresh(token): Promise<UserEntity> {
+    return;
   }
 }
